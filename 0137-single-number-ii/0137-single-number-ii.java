a287f0bec0ac66398fe38b00c7c96874a1890a2e
Yash Shukla <yashshuklaa001@gmail.com>
2025-06-30 22:46:00 +0530

    Time: 4 ms (52.43%), Space: 45.5 MB (65.85%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result=0;
+        for(int i=0;i<32;i++){
+            int count=0;
+            for(int num:nums){
+                if((num>>i & 1) == 1){
+                    count++;
+                }
+            }
+            if(count%3!=0){
+                result=result|(1<<i);
+            }
+        }
+        return result;
+    }
+}
