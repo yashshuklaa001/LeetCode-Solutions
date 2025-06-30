e8e60c20be46463ad43f9343beb7274698edeff3
Yash Shukla <yashshuklaa001@gmail.com>
2025-06-30 23:08:44 +0530

    Time: 0 ms (100%), Space: 42.1 MB (65.83%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,12 +1,36 @@
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         for(int i=0;i<matrix.length;i++){
+//             for(int j=0;j<matrix[0].length;j++){
+//                 if(matrix[i][j]==target){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int low = 0;
+        int high = rows * cols - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int midElement = matrix[mid / cols][mid % cols];
+
+            if (midElement == target) {
+                return true;
+            } else if (midElement < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
+
         return false;
     }
 }
