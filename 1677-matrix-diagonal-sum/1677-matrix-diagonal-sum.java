be4029a30602dab0cca2107d1d284a17d73cb9b8
Yash Shukla <yashshuklaa001@gmail.com>
2025-06-30 23:48:05 +0530

    Time: 0 ms (100%), Space: 44.5 MB (34.93%) - LeetHub

---
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum=0;
+        int n = mat.length;
+        for(int i =0;i<n;i++){
+            sum=sum+mat[i][i];
+            sum=sum+mat[i][n-i-1];
+        }
+        if(n%2==1){
+            sum=sum-mat[n/2][n/2];
+        }
+        return sum;
+    }
+}
