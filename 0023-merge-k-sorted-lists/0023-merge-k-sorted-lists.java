0013cf2f1d0beda20c6e5f31fdf1b4ee6f795eee
Yash Shukla <yashshuklaa001@gmail.com>
2025-07-02 22:56:43 +0530

    Time: 82 ms (14.5%), Space: 44.6 MB (39.98%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoList(ListNode list1,ListNode list2){
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        ListNode head = null;
+        if(list1.val<=list2.val){
+            head=list1;
+            list1=list1.next;
+        }else{
+            head=list2;
+            list2=list2.next;
+        }
+        ListNode temp = head;
+        while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
+                temp.next=list1;
+                list1=list1.next;
+            }else{
+                temp.next=list2;
+                list2=list2.next;
+            }
+            temp=temp.next;
+        }
+        temp.next=(list1==null)?list2:list1;
+        return head;
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length==0){
+            return null;
+        }
+        ListNode result = lists[0];
+        for(int i=1;i<lists.length;i++){
+           result =  mergeTwoList(result,lists[i]);
+        }
+        return result;
+    }
+}
