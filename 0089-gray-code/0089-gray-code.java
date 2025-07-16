3db2301cd66ad424932502eb5e062cf62f678c50
Yash Shukla <yashshuklaa001@gmail.com>
2025-07-17 00:52:07 +0530

    Time: 7 ms (28.78%), Space: 52 MB (12.73%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        result.add(0);
+        for (int i = 0; i < n; i++) {
+            int size = result.size();
+            for (int j = size - 1; j >= 0; j--) {
+                result.add(result.get(j) | (1 << i));
+            }
+        }
+        return result;
+    }
+}
