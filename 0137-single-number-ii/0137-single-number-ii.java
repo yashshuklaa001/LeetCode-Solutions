d95980d1145c2c8c318466904e99cc2ed6622449
Yash Shukla <yashshuklaa001@gmail.com>
2025-07-16 22:32:58 +0530

    Time: 0 ms (100%), Space: 45.3 MB (79.54%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,17 +1,24 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result=0;
-        for(int i=0;i<32;i++){
-            int count=0;
-            for(int num:nums){
-                if((num>>i & 1) == 1){
-                    count++;
-                }
-            }
-            if(count%3!=0){
-                result=result|(1<<i);
-            }
+        // int result=0;
+        // for(int i=0;i<32;i++){
+        //     int count=0;
+        //     for(int num:nums){
+        //         if((num>>i & 1) == 1){
+        //             count++;
+        //         }
+        //     }
+        //     if(count%3!=0){
+        //         result=result|(1<<i);
+        //     }
+        // }
+        // return result;
+        int ones=0;
+        int twos=0;
+        for(int num:nums){
+            ones= (ones ^ num) & ~twos;
+            twos=(twos^num) & ~ones;
         }
-        return result;
+        return ones;
     }
 }
