d3af6ffa0ff5231214018005b762bdebf52d7c7b
Yash Shukla <yashshuklaa001@gmail.com>
2025-07-22 21:26:31 +0530

    Time: 9 ms (87.44%), Space: 45.8 MB (75.46%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -8,34 +8,42 @@
  * }
  */
 public class Codec {
-    public String serialize(TreeNode root) {
-        StringBuilder sb = new StringBuilder();
-        serializeHelper(root, sb);
-        return sb.toString();
-    }
-    private void serializeHelper(TreeNode root, StringBuilder sb) {
+
+    // Helper function to serialize the tree
+    void serializeHelper(TreeNode root, StringBuilder sb) {
         if (root == null) {
-            sb.append("null,");
-            return; // missing in your code
+            sb.append("null").append(",");
+            return;
         }
         sb.append(root.val).append(",");
         serializeHelper(root.left, sb);
         serializeHelper(root.right, sb);
     }
-    public TreeNode deserialize(String data) {
-        String[] arr = data.split(",");
-        Queue<String> queue = new LinkedList<>(Arrays.asList(arr));
-        return deserializeHelper(queue);
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serializeHelper(root, sb);
+        return sb.toString();
     }
-    private TreeNode deserializeHelper(Queue<String> queue) {
+
+    // Helper function to deserialize the tree
+    TreeNode deserializeHelper(Queue<String> queue) {
         String nodeData = queue.poll();
         if (nodeData.equals("null")) {
             return null;
         }
-        TreeNode node = new TreeNode(Integer.parseInt(nodeData));
-        node.left = deserializeHelper(queue);
+        TreeNode node = new TreeNode(Integer.parseInt(nodeData)); // fix 1
+        node.left = deserializeHelper(queue);                     // fix 2
         node.right = deserializeHelper(queue);
         return node;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] arr = data.split(",");
+        Queue<String> queue = new LinkedList<>(Arrays.asList(arr));
+        return deserializeHelper(queue);
     }
 }
 
