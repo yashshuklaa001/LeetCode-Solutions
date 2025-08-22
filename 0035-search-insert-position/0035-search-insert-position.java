2d2fab2313e7052f9eee24bc77b3e39b03c4078e
Yash Shukla <yashshuklaa001@gmail.com>
2025-08-23 00:15:26 +0530

    Time: 0 ms (100%), Space: 43.3 MB (28.03%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,19 +1,62 @@
-class Solution {
+// class Solution {
+//     public int searchInsert(int[] nums, int target) {
+        // int low=0;
+        // int high=nums.length-1;
+        // while(low<=high){
+        //     int mid=(low+high)/2;
+        //     if(nums[mid]==target){
+        //         return mid;
+        //     }
+        //     else if(nums[mid]<target){
+        //         low=mid+1;
+        //     }
+        //     else{
+        //         high=mid-1;
+        //     }
+        // }
+        // return low;
+        // int low=0;
+        // int high=nums.length-1;
+        // while(low<=high){
+        //     int mid=(low+high)/2;
+        //     if(nums[mid]==target){
+        //         return mid;
+        //     }else if(nums[mid]<target){
+        //         low=mid+1;
+        //     }else{
+        //         high=mid-1;
+        //     }
+        // }
+        // return low;
+    class Solution {
     public int searchInsert(int[] nums, int target) {
+        // int low=0;
+        // int high=nums.length-1;
+        // while(low<=high){
+        //     int mid=(low+high)/2;
+        //     if(nums[mid]==target){
+        //         return mid;
+        //     }
+        //     else if(nums[mid]<target){
+        //         low=mid+1;
+        //     }
+        //     else{
+        //         high=mid-1;
+        //     }
+        // }
+        // return low;
         int low=0;
         int high=nums.length-1;
+        int ans=nums.length;
         while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]<target){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                high=mid-1;
+            }else{
                 low=mid+1;
             }
-            else{
-                high=mid-1;
-            }
         }
-        return low;
+        return ans;
     }
 }
