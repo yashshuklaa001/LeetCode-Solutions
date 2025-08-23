1d4429415d9472999bf3580993dd49c4430eb0f9
Yash Shukla <yashshuklaa001@gmail.com>
2025-08-23 16:38:23 +0530

    Time: 0 ms (100%), Space: 50.5 MB (41.18%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,23 +1,36 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        // int low = 0, high = nums.length - 2; // NOTE: high = n - 2
-
+        if(nums.length==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[nums.length-1]!=nums[nums.length-2]) return nums[nums.length-1];
+        int low=1;
+        int high=nums.length-2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
+            if(((mid%2==1) && nums[mid]==nums[mid-1])||(mid%2==0) && nums[mid]==nums[mid+1] ){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return -1;
+        // int low = 0, high = nums.length - 2;
         // while (low <= high) {
         //     int mid = low + (high - low) / 2;
-
-        //     // Check if mid is even or odd and compare with its pair
         //     if (nums[mid] == nums[mid ^ 1]) {
         //         low = mid + 1;
         //     } else {
         //         high = mid - 1;
         //     }
         // }
-
         // return nums[low];
-        int r=0;
-        for(int i:nums){
-            r^=i;
-        }
-        return r;
+        // int r=0;
+        // for(int i:nums){
+        //     r^=i;
+        // }
+        // return r;
     }
 }
