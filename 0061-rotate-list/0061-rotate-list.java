3075c710ac59340acec250a5988e9af2aa8642c4
Yash Shukla <yashshuklaa001@gmail.com>
2025-08-29 21:29:28 +0530

    Time: 0 ms (100%), Space: 42.5 MB (64.31%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,38 +1,29 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         if (head == null || head.next == null || k == 0) return head;
         int length = getLength(head);
         k = k % length;
         if (k == 0) return head;
-        return rotateOnce(head, k);
-    }
-    private int getLength(ListNode head) {
-        int len = 0;
-        while (head != null) {
-            len++;
-            head = head.next;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
         }
-        return len;
-    }
-    private ListNode rotateOnce(ListNode head, int k) {
-        if (k == 0) return head;
-        ListNode prev = null, current = head;
-        while (current.next != null) {
-            prev = current;
-            current = current.next;
+        tail.next = head;
+        int stepsToNewHead = length - k;
+        ListNode newTail = head;
+        for (int i = 1; i < stepsToNewHead; i++) {
+            newTail = newTail.next;
         }
-        prev.next = null;
-        current.next = head;
-        return rotateOnce(current, k - 1);
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+        return newHead;
+    }
+    public int getLength(ListNode start) {
+        int length = 0;
+        while (start != null) {
+            start = start.next;
+            length++;
+        }
+        return length;
     }
 }
